6e50cab778ec16b39ac1c70bd082808f8dc6ad1d
George Stykalin <gstykalin@mail.ru>
2023-05-04 00:39:47 +0300

    update worflow and add supervisor

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 
@@ -7,3 +8,7 @@ app = FastAPI()
 @app.get('/')
 def home():
     return {'status': 'ok'}
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8080)
