01cf070d1ef97cd6aede424490a66a5cb3b12aab
George Stykalin <gstykalin@mail.ru>
2023-05-04 16:03:23 +0300

    update test

--- app/main.py
+++ app/main.py
@@ -9,6 +9,3 @@ app = FastAPI()
 def home():
     return {'status': 'ok'}
 
-
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8080)
--- supervisor/commands/fastapi.sh
+++ supervisor/commands/fastapi.sh
@@ -1,1 +1,1 @@
-uvicorn --workers 1 --host 0.0.0.0 --port 8000 main:app
+uvicorn --workers 1 --host 0.0.0.0 --port 8080 app.main:app
