c3a81482d7e1303efd255446471511755844856d
George Stykalin <gstykalin@mail.ru>
2023-05-09 22:13:30 +0300

    update test

--- tests/test_home.py
+++ tests/test_home.py
@@ -2,7 +2,7 @@ import requests
 
 
 def test_home_route():
-    response = requests.get('http://127.0.0.1:8080/')
+    response = requests.get('http://127.0.0.1:80/')
     data = response.json()
     assert response.status_code == 200
     assert data['status'] == 'ok'
